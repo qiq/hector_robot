8610f578d20e76f7e1ea91ab486e43a50bb3ec7e
Miroslav Spousta <spousta@ufal.mff.cuni.cz>
2010-11-22 09:46:43 +0000

    Return text instead of /etc/passwd.

--- test/http_server.pl
+++ test/http_server.pl
@@ -7,7 +7,7 @@ use HTTP::Status;
 
 # based on the Perl example
 
-my $port = 1235;
+my $port = 8012;
 $port = shift(@ARGV) if (@ARGV > 0);
 
 my $d = HTTP::Daemon->new(
@@ -17,9 +17,13 @@ my $d = HTTP::Daemon->new(
 #print "Please contact me at: <URL:", $d->url, ">\n";
 while (my $c = $d->accept) {
 	while (my $r = $c->get_request) {
-		if ($r->method eq 'GET' and $r->url->path eq "/xyzzy") {
-			# remember, this is *not* recommened practice :-)
-			$c->send_file_response("/etc/passwd");
+		if ($r->method eq 'GET' and $r->url->path eq "/foo.html") {
+			$c->send_basic_header(200);
+			print $c "Content-Type: text/plain";
+			$c->send_crlf;
+			$c->send_crlf;
+			$c->print("bar bar bar BAR!\n");
+			$c->force_last_request();
 		} else {
 			$c->send_error(RC_FORBIDDEN)
 		}
