4d394b633d55fd0c3ec54e47d40065bbab673950
Miroslav Spousta <spousta@ufal.mff.cuni.cz>
2011-08-29 09:37:35 +0000

    Adding word count module.

---
+++ module/WordCount.cc
@@ -0,0 +1,75 @@
+/**
+ */
+#include <config.h>
+
+#ifndef __STDC_FORMAT_MACROS
+#define __STDC_FORMAT_MACROS
+#endif
+
+#include <inttypes.h>
+#include <string.h>
+#include "robot_common.h"
+#include "WordCount.h"
+#include "TextResource.h"
+
+using namespace std;
+
+WordCount::WordCount(ObjectRegistry *objects, const char *id, int threadIndex): Module(objects, id, threadIndex) {
+	items = 0;
+
+	props = new ObjectProperties<WordCount>(this);
+	props->Add("items", &WordCount::GetItems);
+	props->Add("wordCount", &WordCount::GetWordCount);
+
+	nForm = 0;
+	nLemma = 0;
+	nPosTag = 0;
+	nHead = 0;
+	nDepRel = 0;
+}
+
+WordCount::~WordCount() {
+	LOG_DEBUG(this, nForm << ", " << nLemma << ", " << nPosTag << ", " << nHead << ", " << nDepRel);
+	delete props;
+}
+
+char *WordCount::GetItems(const char *name) {
+	return int2str(items);
+}
+
+char *WordCount::GetWordCount(const char *name) {
+	char buf[1024];
+	snprintf(buf, sizeof(buf), "%" PRIu64 ", %" PRIu64 ", %" PRIu64 ", %" PRIu64 ", %" PRIu64, nForm, nLemma, nPosTag, nHead, nDepRel);
+	return strdup(buf);
+}
+
+bool WordCount::Init(vector<pair<string, string> > *params) {
+	// second stage?
+	if (!params)
+		return true;
+
+	if (!props->InitProperties(params))
+		return false;
+
+	return true;
+}
+
+Resource *WordCount::ProcessSimpleSync(Resource *resource) {
+	if (!TextResource::IsInstance(resource))
+		return resource;
+	TextResource *tr = static_cast<TextResource*>(resource);
+
+	nForm += tr->GetFormCount();
+	nLemma += tr->GetLemmaCount();
+	nPosTag += tr->GetPosTagCount();
+	nHead += tr->GetHeadCount();
+	nDepRel += tr->GetDepRelCount();
+
+	return resource;
+}
+
+// factory functions
+
+extern "C" Module* hector_module_create(ObjectRegistry *objects, const char *id, int threadIndex) {
+	return new WordCount(objects, id, threadIndex);
+}
---
+++ module/WordCount.h
@@ -0,0 +1,64 @@
+/**
+WordCount.la, simple, native
+WordCount counts number of words in a resource. Currently, only TextResource is
+supported, but there may be more resource types supported in the future.
+
+Dependencies: none
+
+Parameters:
+items			r/o	Total items processed
+*/
+
+#ifndef _MODULES_WORD_COUNT_H_
+#define _MODULES_WORD_COUNT_H_
+
+#include <config.h>
+
+#include <string>
+#include "common.h"
+#include "Module.h"
+#include "ObjectProperties.h"
+
+class WordCount : public Module {
+public:
+	WordCount(ObjectRegistry *objects, const char *id, int threadIndex);
+	~WordCount();
+	bool Init(std::vector<std::pair<std::string, std::string> > *params);
+	Module::Type GetType();
+	Resource *ProcessSimpleSync(Resource *resource);
+
+private:
+	int items;
+
+	char *GetItems(const char *name);
+	char *GetWordCount(const char *name);
+
+	ObjectProperties<WordCount> *props;
+	char *GetPropertySync(const char *name);
+	bool SetPropertySync(const char *name, const char *value);
+	std::vector<std::string> *ListPropertiesSync();
+
+	uint64_t nForm;
+	uint64_t nLemma;
+	uint64_t nPosTag;
+	uint64_t nHead;
+	uint64_t nDepRel;
+};
+
+inline Module::Type WordCount::GetType() {
+	return SIMPLE;
+}
+
+inline char *WordCount::GetPropertySync(const char *name) {
+	return props->GetProperty(name);
+}
+
+inline bool WordCount::SetPropertySync(const char *name, const char *value) {
+	return props->SetProperty(name, value);
+}
+
+inline std::vector<std::string> *WordCount::ListPropertiesSync() {
+	return props->ListProperties();
+}
+
+#endif
