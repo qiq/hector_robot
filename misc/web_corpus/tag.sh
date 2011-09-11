4e335db0cc015ea384774d27f688120ce23586ee
Miroslav Spousta <spousta@ufal.mff.cuni.cz>
2011-09-11 13:00:46 +0000

    tagging script updated.

--- misc/web_corpus/tag.sh
+++ misc/web_corpus/tag.sh
@@ -5,9 +5,10 @@
 #$ -o log/$JOB_NAME.$JOB_ID.o
 #$ -e log/$JOB_NAME.$JOB_ID.e
 #$ -hard -l mem_free=2g -l act_mem_free=2g -l mnth_free=20g
-#$ -pe make 4
+#$ -p -500
+####$ -pe make 4
 
-echo $HOSTNAME: $JOB_NAME $@
+echo $HOSTNAME: $JOB_NAME $@ >/dev/stderr
 #ulimit -c unlimited
 
 TMP=/mnt/h/tmp
@@ -15,10 +16,10 @@ TMP=/mnt/h/tmp
 . ~/hector_robot/setenv.sh
 . ~/hector_robot/misc/setenv.sh
 
-cp deduplicated/$1.hr $TMP
-hector_server -c WebCorpus.xml -fb tag4 $1 $TMP/$1.hr $TMP/$1.out
-mv $TMP/$1.out tagged/$1.hr
-rm -f $TMP/$1.hr
+cp deduplicated/$1 $TMP
+hector_server -c WebCorpus.xml -fb tag $1 $TMP/$1 $TMP/$1.out
+mv $TMP/$1.out tagged/$1
+rm -f $TMP/$1
 
 #qsub -q "$MACHINE" -hard -l mem_free=12g -l act_mem_free=12g -cwd -S /bin/bash -N "$JOBNAME" bin/experiment.sh "$@"
 #qsub -hard -l mem_free=12g -l act_mem_free=12g -cwd -S /bin/bash -N "$JOBNAME" bin/experiment.sh "$@"
