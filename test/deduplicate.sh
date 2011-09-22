d964d0ebc222115f31d2c62809d0b501bcad67e9
Miroslav Spousta <spousta@ufal.mff.cuni.cz>
2011-09-22 21:09:36 +0000

    Rename deduplicator -> deduplicate.

--- test/deduplicate.sh
+++ test/deduplicate.sh
@@ -2,7 +2,7 @@
 
 . test_common.sh
 
-id=deduplicator
+id=deduplicate
 test_server_batch $id test $id.hr $id.hr.out
 md5sum <$id.hr.out >$id.log.result
 test_compare_result $id
--- test/deduplicate_para.sh
+++ test/deduplicate_para.sh
@@ -2,7 +2,7 @@
 
 . test_common.sh
 
-id=deduplicator_para
+id=deduplicate_para
 test_server_batch $id test $id.hr $id.hr.out
 md5sum <$id.hr.out >$id.log.result
 test_compare_result $id
