4dac93fe66f310535f31104b1d6c5f0510a98f45
Miroslav Spousta <spousta@ufal.mff.cuni.cz>
2011-09-25 11:02:32 +0000

    Premature commit fixed :).

--- module/perl/HectorRobotCommon.pm
+++ module/perl/HectorRobotCommon.pm
@@ -4,12 +4,10 @@ use warnings;
 use strict;
 use utf8;
 
-binmode STDOUT, ":utf8";
 # remove accents from the string
 sub unaccent {
 	my ($s) = @_;
 	return $s unless ($s =~ m/[^[:ascii:]]/);
-	$s =~ tr/č/c/;
 	$s =~ tr/ÀÁÂÃÄÅàáâãäåÇçČčĎďÈÉÊËèéêëĚěÌÍÎÏìíîïĽľŇňÑñÒÓÔÕÖØòóôõöøŘřŠšŤťÙÚÛÜùúûüŮůÝÿýŽž/AAAAAAaaaaaaCcCcDdEEEEeeeeEeIIIIiiiiLlNnNnOOOOOOooooooRrSsTtUUUUuuuuUuYyyZz/;
 	my %trans = (
 		'Æ' => 'AE',
