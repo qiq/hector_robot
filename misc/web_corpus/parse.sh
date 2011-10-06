348ec16eec07192a4ee38895cbf3ee264e128a6b
Miroslav Spousta <spousta@ufal.mff.cuni.cz>
2011-10-06 12:44:19 +0000

    Running tagging, updates.

--- misc/web_corpus/parse.sh
+++ misc/web_corpus/parse.sh
@@ -9,6 +9,7 @@
 ##$ -pe make 4
 
 echo $HOSTNAME: $JOB_NAME $@ >/dev/stderr
+renice 10 $$
 #ulimit -c unlimited
 
 TMP=/mnt/h/tmp
@@ -16,10 +17,10 @@ TMP=/mnt/h/tmp
 . ~/hector_robot/setenv.sh
 . ~/hector_robot/misc/setenv.sh
 
-cp tagged/$1 $TMP
-hector_server -c WebCorpus.xml -fb parse $1 $TMP/$1 $TMP/$1.out
-mv $TMP/$1.out parsed/$1
-rm -f $TMP/$1
+cp $1 $TMP/$$.tmp
+hector_server -c WebCorpus.xml -fb parse $TMP/$$.tmp $TMP/$$.tmp.out $2.log
+mv $TMP/$$.tmp.out $2
+rm -f $TMP/$$.tmp
 
 #qsub -q "$MACHINE" -hard -l mem_free=12g -l act_mem_free=12g -cwd -S /bin/bash -N "$JOBNAME" bin/experiment.sh "$@"
 #qsub -hard -l mem_free=12g -l act_mem_free=12g -cwd -S /bin/bash -N "$JOBNAME" bin/experiment.sh "$@"
--- misc/web_corpus/tag.sh
+++ misc/web_corpus/tag.sh
@@ -9,6 +9,7 @@
 ####$ -pe make 4
 
 echo $HOSTNAME: $JOB_NAME $@ >/dev/stderr
+renice 10 $$
 #ulimit -c unlimited
 
 TMP=/mnt/h/tmp
@@ -16,10 +17,10 @@ TMP=/mnt/h/tmp
 . ~/hector_robot/setenv.sh
 . ~/hector_robot/misc/setenv.sh
 
-cp deduplicated/$1 $TMP
-hector_server -c WebCorpus.xml -fb tag $1 $TMP/$1 $TMP/$1.out
-mv $TMP/$1.out tagged/$1
-rm -f $TMP/$1
+cp $1 $TMP/$$.tmp
+hector_server -c WebCorpus.xml -fb tag $TMP/$$.tmp $TMP/$$.tmp.out $2.log
+mv $TMP/$$.tmp.out $2
+rm -f $TMP/$$.tmp
 
 #qsub -q "$MACHINE" -hard -l mem_free=12g -l act_mem_free=12g -cwd -S /bin/bash -N "$JOBNAME" bin/experiment.sh "$@"
 #qsub -hard -l mem_free=12g -l act_mem_free=12g -cwd -S /bin/bash -N "$JOBNAME" bin/experiment.sh "$@"
