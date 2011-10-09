c79fe2a527b0f9037888bfc523a189df18456a64
Miroslav Spousta <spousta@ufal.mff.cuni.cz>
2011-10-09 06:59:29 +0000

    Adding LanguageCount module.

---
+++ module/LanguageCount.cc
@@ -0,0 +1,83 @@
+/**
+ */
+#include <config.h>
+
+#ifndef __STDC_FORMAT_MACROS
+#define __STDC_FORMAT_MACROS
+#endif
+
+#include <inttypes.h>
+#include <string.h>
+#include "robot_common.h"
+#include "LanguageCount.h"
+#include "TextResource.h"
+
+using namespace std;
+
+LanguageCount::LanguageCount(ObjectRegistry *objects, const char *id, int threadIndex): Module(objects, id, threadIndex) {
+	items = 0;
+
+	props = new ObjectProperties<LanguageCount>(this);
+	props->Add("items", &LanguageCount::GetItems);
+	props->Add("languageCount", &LanguageCount::GetLanguageCount);
+}
+
+LanguageCount::~LanguageCount() {
+	char *lc = GetLanguageCount("languageCount");
+	LOG_DEBUG(this, lc);
+	free(lc);
+	delete props;
+}
+
+char *LanguageCount::GetItems(const char *name) {
+	return int2str(items);
+}
+
+char *LanguageCount::GetLanguageCount(const char *name) {
+	string result;
+	for (tr1::unordered_map<string, uint64_t>::iterator iter = count.begin(); iter != count.end(); ++iter) {
+		char s[100];
+		snprintf(s, sizeof(s), "%s %"PRIu64"\n", iter->first.c_str(), iter->second);
+		result.append(s);
+	}
+	return strdup(result.c_str());
+}
+
+bool LanguageCount::Init(vector<pair<string, string> > *params) {
+	// second stage?
+	if (!params)
+		return true;
+
+	if (!props->InitProperties(params))
+		return false;
+
+	return true;
+}
+
+Resource *LanguageCount::ProcessSimpleSync(Resource *resource) {
+	if (!TextResource::IsInstance(resource))
+		return resource;
+	TextResource *tr = static_cast<TextResource*>(resource);
+
+	string langs = tr->GetLanguage();
+	vector<string> v;
+	splitOnWs(v, langs);
+	for (vector<string>::iterator iter = v.begin(); iter != v.end(); ++iter) {
+		tr1::unordered_map<string, uint64_t>::iterator iter2 = count.find(*iter);
+		if (iter2 != count.end()) {
+			iter2->second++;
+		} else {
+			count[*iter] = 1;
+		}
+	}
+
+	items++;
+
+	return resource;
+}
+
+// factory functions
+
+extern "C" Module* hector_module_create(ObjectRegistry *objects, const char *id, int threadIndex) {
+	return new LanguageCount(objects, id, threadIndex);
+}
---
+++ module/LanguageCount.h
@@ -0,0 +1,59 @@
+/**
+LanguageCount.la, simple, native
+LanguageCount counts number of languages in TextResources.
+
+Dependencies: none
+
+Parameters:
+items			r/o	Total items processed
+*/
+
+#ifndef _MODULES_WORD_COUNT_H_
+#define _MODULES_WORD_COUNT_H_
+
+#include <config.h>
+
+#include <string>
+#include "common.h"
+#include "Module.h"
+#include "ObjectProperties.h"
+
+class LanguageCount : public Module {
+public:
+	LanguageCount(ObjectRegistry *objects, const char *id, int threadIndex);
+	~LanguageCount();
+	bool Init(std::vector<std::pair<std::string, std::string> > *params);
+	Module::Type GetType();
+	Resource *ProcessSimpleSync(Resource *resource);
+
+private:
+	int items;
+
+	char *GetItems(const char *name);
+	char *GetLanguageCount(const char *name);
+
+	ObjectProperties<LanguageCount> *props;
+	char *GetPropertySync(const char *name);
+	bool SetPropertySync(const char *name, const char *value);
+	std::vector<std::string> *ListPropertiesSync();
+
+	std::tr1::unordered_map<std::string, uint64_t> count;
+};
+
+inline Module::Type LanguageCount::GetType() {
+	return SIMPLE;
+}
+
+inline char *LanguageCount::GetPropertySync(const char *name) {
+	return props->GetProperty(name);
+}
+
+inline bool LanguageCount::SetPropertySync(const char *name, const char *value) {
+	return props->SetProperty(name, value);
+}
+
+inline std::vector<std::string> *LanguageCount::ListPropertiesSync() {
+	return props->ListProperties();
+}
+
+#endif
