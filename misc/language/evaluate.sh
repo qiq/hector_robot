0c1120607e1977c5763f117b5753fb5f3b2e6607
Miroslav Spousta <spousta@ufal.mff.cuni.cz>
2011-10-11 10:41:30 +0000

    Evaluation script, first draft.

---
+++ misc/language/evaluate.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#for l in af ar az be bg bn ca cs cy da de el en es et eu fa fi fr gl he hi hr hu id is it ka lt lv mk ml ms nl pl pt ro ru sk sl sq sr sv ta te th tr uk zh; do
+>evaluate_gcld.txt
+>evaluate_top_words.txt
+for l in cs de en sk; do
+	hector_server -c Language.xml -fb test_gcld data/${l}_test.hr.gz 2>&1 | cat >>evaluate_gcld.txt
+	hector_server -c Language.xml -fb test_top_words data/${l}_test.hr.gz 2>&1 | cat >>evaluate_top_words.txt
+done
+rm -f /tmp/delme.$$
