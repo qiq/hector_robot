c895656cdef198f5a6a9122f40176b44231756b9
Miroslav Spousta <spousta@ufal.mff.cuni.cz>
2011-10-11 19:23:06 +0000

    train+eval should work now.

--- misc/language/evaluate.sh
+++ misc/language/evaluate.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-#for l in af ar az be bg bn ca cs cy da de el en es et eu fa fi fr gl he hi hr hu id is it ka lt lv mk ml ms nl pl pt ro ru sk sl sq sr sv ta te th tr uk zh; do
->evaluate_gcld.txt
+#>evaluate_gcld.txt
 >evaluate_top_words.txt
+#for l in af ar az be bg bn ca cs cy da de el en es et eu fa fi fr gl he hi hr hu id is it ka lt lv mk ml ms nl pl pt ro ru sk sl sq sr sv ta te th tr uk zh; do
 for l in cs de en sk; do
-	hector_server -c Language.xml -fb test_gcld data/${l}_test.hr.gz 2>&1 | cat >>evaluate_gcld.txt
-	hector_server -c Language.xml -fb test_top_words data/${l}_test.hr.gz 2>&1 | cat >>evaluate_top_words.txt
+	#hector_server -c Language.xml -fb test_gcld data.old/${l}_test.hr.gz 2>&1 | cat >>evaluate_gcld.txt
+	hector_server -c Language.xml -fb test_top_words data.old/${l}_test.hr.gz data.old/top_words $l >>evaluate_top_words.txt 2>&1
 done
 rm -f /tmp/delme.$$
--- misc/language/prepare_data.sh
+++ misc/language/prepare_data.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-for f in text/*; do
-#for f in text/ug.bz2; do
+#for l in af ar az be bg bn ca cs cy da de el en es et eu fa fi fr gl he hi hr hu id is it ka lt lv mk ml ms nl pl pt ro ru sk sl sq sr sv ta te th tr uk zh; do
+for l in cs de en sk; do
 	mkdir split 2>/dev/null
-	l=`echo $f|sed -e 's/text\/\(.*\)\.bz2/\1/'`
-	bzip2 -dc <$f|split -d -l 10000 -a 5 - split/$l.
+	bzip2 -dc <text/$l.bz2|split -d -l 10000 -a 5 - split/$l.
 	find split -type f >data/$l.list
 	n=`wc -l data/$l.list|cut -f1 -d' '`
 	if [ $n -gt 100 ]; then
@@ -16,7 +15,7 @@ for f in text/*; do
 	head -n $tr data/$l.list >data/$l.train.list
 	tail -n $te data/$l.list >data/$l.test.list
 
-	hector_server -c Language.xml -fb prepare_train_data data/$l.train.list data/${l}_train.hr.gz "$l ?" data/top_words.$l
+	hector_server -c Language.xml -fb prepare_train_data data/$l.train.list data/${l}_train.hr.gz "$l ?" data/top_words.$l 100000
 	hector_server -c Language.xml -fb prepare_test_data data/$l.test.list data/${l}_test.hr.gz
 
 	rm -rf split
