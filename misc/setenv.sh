695124f1495b272856f0f0680c583c5ed588c4fc
Miroslav Spousta <spousta@ufal.mff.cuni.cz>
2011-10-13 06:50:00 +0000

    Some notes added (mainly numbers ;-)).

--- misc/setenv.sh
+++ misc/setenv.sh
@@ -4,6 +4,8 @@ base=`dirname "$base"`
 base=`readlink -f "$base"`'/..'
 base=`readlink -f "$base"`
 
+. $base/setenv.sh
+
 export "LD_LIBRARY_PATH=$base/module/.libs:$base/resource/.libs:$base/server/.libs:$base/perl/.libs:$base/python/.libs:$LD_LIBRARY_PATH"
 export "PERL5LIB=$base/perl:$base/module/perl:$base/resource/perl:$PERL5LIB"
 export "PYTHONPATH=$base/python:$base/module/python:$base/resource/python:$PYTHONPATH"
