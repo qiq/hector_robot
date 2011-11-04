504ef9c783638e93df06c5ad6c5d1c6811d40ddd
Miroslav Spousta <spousta@ufal.mff.cuni.cz>
2011-11-04 09:17:42 +0000

    Tools update, notes update.

--- misc/web_corpus/parse.sh
+++ misc/web_corpus/parse.sh
@@ -4,23 +4,21 @@
 #$ -N parse_mst
 #$ -o log/$JOB_NAME.$JOB_ID.o
 #$ -e log/$JOB_NAME.$JOB_ID.e
-#$ -hard -l mem_free=8g -l act_mem_free=8g -l mnth_free=20g
+#$ -hard -l mem_free=10g -l act_mem_free=10g -l h_vmem=10g -l mnth_free=20g
 #$ -p -500
-##$ -pe make 4
 
-echo $HOSTNAME: $JOB_NAME $@ >/dev/stderr
-renice 10 $$
-#ulimit -c unlimited
+echo $HOSTNAME: $JOB_NAME $@ >>/dev/stderr
+renice 10 $$ >/dev/null
 
 TMP=/mnt/h/tmp
 
-. ~/hector_robot/setenv.sh
-. ~/hector_robot/misc/setenv.sh
+. ../setenv.sh
 
 cp $1 $TMP/$$.tmp
-hector_server -c WebCorpus.xml -fb parse $TMP/$$.tmp $TMP/$$.tmp.out $2.log
+hector_server -c WebCorpus.xml -fb parse $TMP/$$.tmp $TMP/$$.tmp.out $2.log &
+pid=$!
+echo "hector_server: $pid" >>/dev/stderr
+wait $pid
+echo "result: $?" >>/dev/stderr
 mv $TMP/$$.tmp.out $2
 rm -f $TMP/$$.tmp
-
-#qsub -q "$MACHINE" -hard -l mem_free=12g -l act_mem_free=12g -cwd -S /bin/bash -N "$JOBNAME" bin/experiment.sh "$@"
-#qsub -hard -l mem_free=12g -l act_mem_free=12g -cwd -S /bin/bash -N "$JOBNAME" bin/experiment.sh "$@"
--- misc/web_corpus/tag.sh
+++ misc/web_corpus/tag.sh
@@ -6,21 +6,15 @@
 #$ -e log/$JOB_NAME.$JOB_ID.e
 #$ -hard -l mem_free=2g -l act_mem_free=2g -l mnth_free=20g
 #$ -p -500
-####$ -pe make 4
 
-echo $HOSTNAME: $JOB_NAME $@ >/dev/stderr
-renice 10 $$
-#ulimit -c unlimited
+echo $HOSTNAME: $JOB_NAME $@ >>/dev/stderr
+renice 10 $$ >/dev/null
 
 TMP=/mnt/h/tmp
 
-. ~/hector_robot/setenv.sh
-. ~/hector_robot/misc/setenv.sh
+. ../setenv.sh
 
 cp $1 $TMP/$$.tmp
 hector_server -c WebCorpus.xml -fb tag $TMP/$$.tmp $TMP/$$.tmp.out $2.log
 mv $TMP/$$.tmp.out $2
 rm -f $TMP/$$.tmp
-
-#qsub -q "$MACHINE" -hard -l mem_free=12g -l act_mem_free=12g -cwd -S /bin/bash -N "$JOBNAME" bin/experiment.sh "$@"
-#qsub -hard -l mem_free=12g -l act_mem_free=12g -cwd -S /bin/bash -N "$JOBNAME" bin/experiment.sh "$@"
